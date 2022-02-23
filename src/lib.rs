3675291ec448db05add8b42bf8636e0aff5b7061
Yamamoto Kiyotaka <yamamotokiy@kawai.co.jp>
2022-02-23 14:29:07 +0900

    (2) fail test case

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        // このテストを失敗させる
+        panic!("Make this test fail");
     }
 }
