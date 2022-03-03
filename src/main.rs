1235b0f210fb18c2481ba51d6220b23e7e9d2bba
Yamamoto Kiyotaka <yamamotokiy@kawai.co.jp>
2022-03-03 15:00:19 +0900

    参照と借用の問題で動かない

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,24 @@
 use rand::Rng;
 
 fn main() {
-    const ARRAY_SIZE: usize = 100;
+    const ARRAY_SIZE: usize = 5;
     let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE]; 
     let mut count = 0;
     loop {
-        if count > ARRAY_SIZE-1 { break; }
+        if count > ARRAY_SIZE-1 {
+            println!();
+            break;
+        }
         array[count] = rand::thread_rng().gen_range(0, 1001);
-        println!("配列{}: {}", count, array[count]);
+        print!("[{}] ", array[count]);
         count+=1;
+    }
+
+    for (i, &element) in array.iter().enumerate() {
+        if i > ARRAY_SIZE-2 { break; }
+        if element >= array[i+1] {
+            array.swap(i, i+1);
+        }
+        println!("i: {}, elm: {}, next: {}", i, element, array[i+1]);
     }
 }
