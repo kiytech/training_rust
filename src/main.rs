317f0737373fe2dc231bf96610ca5edfb93d34d3
Yamamoto Kiyotaka <yamamotokiy@kawai.co.jp>
2022-03-17 18:30:03 +0900

    update: ランダム配列の生成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ ** 選択ソート
+ ** -1000 以上 1000 以下のランダムな整数を100回発生させ、その整数を昇順に並び替える。
+*/
+use rand::Rng;
+
+const ARRAY_SIZE: usize = 5;
 fn main() {
-    println!("Hello, world!");
+    let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE];
+    let mut count = 0;
+    print!("Init: ");
+    loop {
+        if count > ARRAY_SIZE-1 {
+            println!();
+            break;
+        }
+        array[count] = rand::thread_rng().gen_range(0, 1001);
+        print!("[{}] ", array[count]);
+        count+=1;
+    }
 }
