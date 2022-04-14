ad1bc1de7b220b01c3701d86de7fe9da02dcfffe
Yamamoto Kiyotaka <yamamotokiy@example.co.jp>
2022-04-14 16:55:36 +0900

    update: 親ノードと子ノードを比較してスワップする関数を実装

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,50 @@ const ARRAY_SIZE: usize = 10;
 fn main() {
     let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE];
     let mut count = 0;
-    while count < ARRAY_SIZE-1 {
+    while count < ARRAY_SIZE {
         array[count] = rand::thread_rng().gen_range(0..100);
         count+=1;
     }
-    heap_sort(array.to_vec());
+
     println!("Generate rand array: {:?}", array);
-    println!("Max: {:?}", ind_max(array.to_vec()));
+    println!("Swaped array       : {:?}", heap_sort(array.to_vec()));
+}
+
+struct NodesIdx {
+    parent: usize,
+    child: usize
+}
+impl NodesIdx {
+    fn set_parent(&mut self, idx: usize) {
+        self.parent = idx; 
+        self.calc_child_idx();
+    }
+    fn set_child(&mut self, idx: usize) {
+        self.child = idx; 
+    }
+    fn calc_last_parent_idx(&mut self, len:usize) {
+        self.set_parent((len / 2) -1);
+    }
+    fn calc_child_idx(&mut self) {
+        self.set_child(self.parent * 2 + 1);
+    }
 }
 
 fn heap_sort(v:Vec<i32>) -> Vec<i32> {
-    return v
+    let mut nodes_idx = NodesIdx {parent: 0, child: 0};
+    nodes_idx.calc_last_parent_idx(v.len());
+    println!("parent_idx: {}, child_idx: {}", nodes_idx.parent, nodes_idx.child);
+    nodes_swap(v, nodes_idx)
+}
+
+fn nodes_swap(mut v:Vec<i32>, mut nodes_idx:NodesIdx) -> Vec<i32> {
+    for _i in 0..1 {
+        if v[nodes_idx.parent] < v[nodes_idx.child] {
+            v.swap(nodes_idx.parent, nodes_idx.child);
+        }
+        nodes_idx.child = nodes_idx.child + 1;
+    }
+    v
 }
 
 #[allow(dead_code)]
