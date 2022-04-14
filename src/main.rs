47550270ae6ce971a23f9add273dea8159f3976b
Yamamoto Kiyotaka <yamamotokiy@example.co.jp>
2022-04-14 13:42:07 +0900

    update: ひとまず必要な素材を準備
    - 乱数生成
    - 最小値の計算

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+
+const ARRAY_SIZE: usize = 10;
 fn main() {
-    println!("Hello, world!");
+    let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE];
+    let mut count = 0;
+    loop {
+        if count > ARRAY_SIZE-1 {
+            break;
+        }
+        array[count] = rand::thread_rng().gen_range(0..100);
+        count+=1;
+    }
+    heap_sort(array.to_vec());
+    println!("Generate rand array: {:?}", array);
+}
+
+fn heap_sort(v:Vec<i32>) -> Vec<i32> {
+    return v
+}
+
+#[allow(dead_code)]
+fn min_ind(v:Vec<i32>) -> (usize, i32) {
+    v.iter()
+        .enumerate()
+        .fold((usize::MAX, i32::MAX), |(i_a, a), (i_b, &b)| {
+            if b < a {
+                (i_b, b)
+            } else {
+                (i_a, a)
+            }
+        })
 }
